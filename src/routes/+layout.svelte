5753ab57d1c4e1b1db97e5de7314c8db35c18dc5
px <m@mamoco.se>
2024-10-24 15:22:17 +0200

    basic skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+	import Header from '$lib/components/header/Header.svelte';
 	import '../app.css';
-	let { children } = $props();
+
+	let { children, data } = $props();
 </script>
 
-{@render children()}
+<Header
+	data={data}
+/>
+
+
+<main class="px-6 ">
+	{@render children?.()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+    import type { PageServerData } from './$types';
+
+    let { data }: { data: PageServerData } = $props();
+
+    console.log(data)
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
