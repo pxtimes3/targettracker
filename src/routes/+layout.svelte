40f2c81dfa04a4fed7260ad4de1fd5380bea9053
px <m@mamoco.se>
2024-10-25 21:33:46 +0200

    housekeeping

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-	import Header from '$lib/components/header/Header.svelte';
+	import Header from '@/components/header/header.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+	import Footer from '@/components/footer/footer.svelte';
 
 	let { children, data } = $props();
 </script>
@@ -10,6 +12,11 @@
 />
 
 
-<main class="px-6 ">
+<main class="px-8 pt-4">
+	<div class="container maxw-screen-2x1 items-center">
+	<ModeWatcher />
 	{@render children?.()}
+	</div>
 </main>
+
+<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,4 @@
 
     console.log(data)
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to Target Tracker!</h1>
