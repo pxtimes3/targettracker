0b02a28ea2b0781299a54b27dea1b3fde7d0e293
px <m@mamoco.se>
2024-11-04 19:52:09 +0100

    FAQ-table. RLS.

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,10 +1,13 @@
 import { sql } from "drizzle-orm";
-import { boolean, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { boolean, pgEnum, pgPolicy, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const rolesEnum = pgEnum("roles", ["user", "vip", "admin"]);
 
 export const user = pgTable('user', {
 	id: uuid('id').primaryKey(),
 	username: text('username').notNull().unique(),
 	passwordHash: text('password_hash').notNull(),
+	role: rolesEnum().default("user"),
 	email: text('email').notNull().unique(),
 	invitedBy: uuid('invitedBy'),
 	inviteCode: uuid('inviteCode'),
@@ -31,6 +34,29 @@ export const invitecodes = pgTable("invitecodes", {
 	active: boolean("active").default(true)
 });
 
+export const faq = pgTable("faq", {
+	id: uuid("id").defaultRandom(),
+	question: text("question").notNull(),
+	answer: text("answer").notNull(),
+	created: timestamp("created").default(sql`now()`),
+	updated: timestamp("updated").default(sql`now()`).$onUpdate(() => new Date()),
+}, (t) => [
+	pgPolicy('admin', {
+		as: 'permissive',
+		to: 'admin',
+		for: 'all',
+		using: sql``,
+		withCheck: sql``,
+	}),
+	pgPolicy('public can view', {
+		as: 'permissive',
+		to: undefined,
+		for: 'select',
+		using: sql``,
+	}),
+]);
+
 export type Session = typeof session.$inferSelect;
 export type User = typeof user.$inferSelect;
 export type InviteCodes = typeof invitecodes.$inferSelect;
+export type Faq = typeof faq.$inferSelect;
