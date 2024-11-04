266fa2ab27919f5fe699d2d91472d096fc52aaef
px <m@mamoco.se>
2024-11-04 21:53:42 +0100

    Housekeeping.

--- src/routes/faq/+page.server.ts
+++ src/routes/faq/+page.server.ts
@@ -7,11 +7,12 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async () => {
     try {
         const query = await db.select()
-            .from(faq);
+            .from(faq)
+            .orderBy(faq.order);
 
         if (query) {
             console.log(query)
-            return { faq: query[0] }
+            return { faq: query }
         }
     } catch (error) {
         // FIXME: Logging
@@ -20,18 +21,5 @@ export const load: PageServerLoad = async () => {
 };
 
 export const actions = {
-    targetupload: async ({ request }) => {
-        console.log('targetupload called');
-        const formData = await request.formData();
-        if (!formData) return;
 
-        const uploadedFile = formData?.get('file') as File;
-        if(!uploadedFile) return;
-
-        const filename = `${uuidv4()}${extname(uploadedFile?.name)}`;
-        console.log(filename);
-        // await writeFile(filename, Buffer.from(await uploadedFile?.arrayBuffer()));
-
-        return { success: true };
-    }
 };
--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,67 +1,31 @@
 <script lang="ts">
     import { Accordion } from '@skeletonlabs/skeleton-svelte';
     import { CircleHelp } from 'lucide-svelte';
-    const value = $state(['item-1']);
+    import type { PageData } from './$types';
+
+    const value: string[]|undefined = $state(['item-0']);
+
+    let { data } : { data: PageData } = $props();
 </script>
 
-<div>
-    <div class="grid grid-cols-[1fr,_5fr] gap-4 mt-8">
-        <div class="col-span-2 text-2xl mb-4"><h1>Frequently Asked Questions</h1></div>
-        <div>Contents</div>
+
+<div class="grid grid-cols-[1fr,_5fr] gap-4 mt-8">
+    <div class="col-span-2 text-2xl mb-4"><h1>Frequently Asked Questions</h1></div>
+    <div>Contents</div>
+    {#if data?.faq?.length}
+    <div class="bg-surface-700 divide-surface-200-800 block divide-y overflow-hidden">
         <Accordion {value} multiple>
-            <Accordion.Item value="item-1">
-                {#snippet lead()}<CircleHelp />{/snippet}
-                {#snippet control()}What is this?{/snippet}
-                {#snippet panel()}
-                    <p>Target Tracker is a tool for you as a target shooter to keep track of your progression. You can easily see historical data, sorted on firearm, ammunition, etc. and answer questions such as "Did my 30-06 shoot better with a certain load?".</p>
-                    <p>If you're a hand loader you can also keep track of your load data and compare them with each other.</p>
-                {/snippet}
-            </Accordion.Item>
-            <hr class="hr" />
-            <Accordion.Item value="item-2">
-                {#snippet lead()}<CircleHelp />{/snippet}
-                {#snippet control()}Is it safe?{/snippet}
-                {#snippet panel()}
-                    <p>Yes, as safe as it can be. All data is anonymized to the fullest extent it can be. As long as you as a user don't upload targets with personal data & keep your login information safe it's safe enough.</p>
-                {/snippet}
-            </Accordion.Item>
-            <hr class="hr" />
-            <Accordion.Item value="item-3">
-                {#snippet lead()}<CircleHelp />{/snippet}
-                {#snippet control()}Can I show my sub-half-MOA group to my friends?{/snippet}
-                {#snippet panel()}
-                    <p>Of course! You can set your profile to "public" which creates an URL to your page with targets you have marked as public. You also get an unlisted URL to link to your friends, forums etc. when you save a target.</p>
-                {/snippet}
-            </Accordion.Item>
-            <hr class="hr" />
-            <Accordion.Item value="item-4">
-                {#snippet lead()}<CircleHelp />{/snippet}
-                {#snippet control()}Why is this free?{/snippet}
-                {#snippet panel()}
-                    <p>Information wants to be free!</p>
-                    <p>This is first and foremost a way for me to learn new techniques in programming. It started out as a "Let's learn Svelte" project to keep track of my off season air-gun training.</p>
-                    <p>There is of course an option to donate to cover server costs, but the philosophny is that it should not be necessary to access the features of Target Tracker.</p>
-                    <p>In the future there may be some sort of "VIP" membership with extra perks, but my aim is to not enshittificate the basic users experience just to make a buck.</p>
-                {/snippet}
-            </Accordion.Item>
-            <hr class="hr" />
-            <Accordion.Item value="item-5">
-                {#snippet lead()}<CircleHelp />{/snippet}
-                {#snippet control()}Do you sell or share any data?{/snippet}
-                {#snippet panel()}
-                    <p>No!</p>
-                    <p>The only data "shared" is a low resolution, black and white, target image used for training a LLM in order to be able to create groups on the fly for users. That data remains a property of Mamoco AB and will not be re-sold to any party without your specific consent.</p>
-                {/snippet}
-            </Accordion.Item>
-            <hr class="hr" />
-            <Accordion.Item value="item-6">
-                {#snippet lead()}<CircleHelp />{/snippet}
-                {#snippet control()}The logo is silly!{/snippet}
-                {#snippet panel()}
-                    <p>Yes!</p>
-                {/snippet}
-            </Accordion.Item>
-            <hr class="hr" />
+            {#each data.faq as faq, i}
+                <Accordion.Item value="item-{i}">
+                    {#snippet lead()}<CircleHelp />{/snippet}
+                    {#snippet control()}{faq.question}{/snippet}
+                    {#snippet panel()}
+                        <p>{@html faq.answer.replaceAll(/(\r\n)+/gi,'</p><p>')}</p>
+                    {/snippet}
+                </Accordion.Item>
+                <hr class="hr border-t-2" />
+            {/each}
         </Accordion>
     </div>
+    {/if}
 </div>
