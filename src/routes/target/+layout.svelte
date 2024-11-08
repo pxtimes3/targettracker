f2769f521ff1de137d61a244bcfe8f5a5c545647
px <m@mamoco.se>
2024-11-08 13:57:08 +0100

    flyttade +layout.svelte till resp. folder för att kunna göra fullscreen om det är en mobil + kamera osv...

---
+++ src/routes/target/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Footer from '@/components/footer/footer.svelte';
+	import Header from '@/components/header/header.svelte';
+	import '../../app.css';
+
+	let { children, data } = $props();
+</script>
+
+<Header
+	data={data}
+/>
+
+<main class="container pt-4 grid grid-flow-col grid-cols-1 justify-center justify-self-center">
+	<div class="container grid h-full w-full">
+
+	{@render children?.()}
+	</div>
+</main>
+
+<Footer />
