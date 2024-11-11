9f33719eb0d331fee12bf03a6687ca40ee7d3701
px <m@mamoco.se>
2024-11-11 17:33:56 +0100

    deleted

--- src/routes/auth/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const loginForm = z.object({
-    username: z.string().min(2).max(50),
-    password: z.string().min(8),
-});
-
-export type FormSchema = typeof loginForm;
