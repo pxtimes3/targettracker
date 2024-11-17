ce9aae939c0d97a971260433aa686d2cefdea5e1
px <m@mamoco.se>
2024-11-17 18:25:47 +0100

    flaggor för editorn

---
+++ src/lib/stores/EditorStore.ts
@@ -0,0 +1,31 @@
+/**
+ * Pubsub-ish för target editorn.
+ */
+import { type Writable, writable } from "svelte/store";
+import { z } from 'zod';
+
+const WarningSchema = z.object({
+    id: z.string(),
+    message: z.string()
+});
+
+export type WarningInterface = z.infer<typeof WarningSchema>;
+
+const EditorStoreSchema = z.object({
+    warnings: z.array(WarningSchema).optional(),
+})
+
+export type EditorStoreInterface = z.infer<typeof EditorStoreSchema>;
+
+export const activeButton:      Writable<string> = writable();
+export const activePanel:       Writable<string> = writable();
+
+export const showAddFirearm:    Writable<boolean> = writable(false);
+export const showAddAmmunition: Writable<boolean> = writable(false);
+export const showAddWeather:    Writable<boolean> = writable(false);
+
+const warnings: Writable<WarningInterface[]> = writable([]);
+
+export const EditorStore: Writable<EditorStoreInterface> = writable({
+    warnings: []
+});
