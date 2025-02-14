6bf7989b842079b45009b3515dfdec8487f05381
px <m@mamoco.se>
2025-02-14 16:15:15 +0100

    FlowBite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,19 @@ import type { Config } from 'tailwindcss';
 export default {
     content: [
         './src/**/*.{html,js,svelte,ts}',
-        // contentPath(import.meta.url, 'svelte')
+        "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
     ],
     theme: {
-        extend: {},
+        extend: {
+			colors: {
+        		// flowbite-svelte
+        		primary: { 50: '#FFF5F2', 100: '#FFF1EE', 200: '#FFE4DE', 300: '#FFD5CC', 400: '#FFBCAD', 500: '#FE795D', 600: '#EF562F', 700: '#EB4F27', 800: '#CC4522', 900: '#A5371B'},
+      		}
+		},
     },
     plugins: [
-        // skeleton({
-            // NOTE: each theme included will increase the size of your CSS bundle
-            // themes: [ themes.cerberus, themes.wintry ]
-        // })
-    ]
+        require('flowbite/plugin')
+    ],
+	darkMode: 'class',
+
 } satisfies Config
