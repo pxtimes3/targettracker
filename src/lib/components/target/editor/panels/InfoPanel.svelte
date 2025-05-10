bda53fe95ca58c38350b0fe017fc830a865153e2
Magnus Sandell <m@mamoco.se>
2025-05-10 20:57:42 +0200

    Inline => Egna Komponenter

--- src/lib/components/target/editor/panels/InfoPanel.svelte
+++ src/lib/components/target/editor/panels/InfoPanel.svelte
@@ -4,148 +4,32 @@
 		TODO: Val av firearm i infopanel ska utesluta kalibrar som inte passar.
 	*/
 	import type { PageServerData } from '$types';
-	import { Button, Field, Input, MenuItem, Popover, SelectField, TextField, Toggle, type MenuOption } from 'svelte-ux';
 	import { slide } from 'svelte/transition'
 	import { quadInOut } from 'svelte/easing';
 	import { DateTime } from "luxon";
 	import { EditorStore } from '@/stores/EditorStore';
 	import { TargetStore, type TargetStoreInterface } from '@/stores/TargetImageStore';
-	import { CircleHelp, PlusSquare } from 'lucide-svelte';
 	import { onMount } from 'svelte';
-	import { cls } from '@layerstack/tailwind';
-	import type { UUIDTypes } from 'uuid';
+	import Visibility from './InfoPanel/Visibility.svelte';
+	import Note from './InfoPanel/Note.svelte';
+	import Ammunition from './InfoPanel/Ammunition.svelte';
+	import Firearm from './InfoPanel/Firearm.svelte';
+	import Distance from './InfoPanel/Distance.svelte';
+	import Name from './InfoPanel/Name.svelte';
+	import TargetType from './InfoPanel/TargetType.svelte';
+	import Event from './InfoPanel/Event.svelte';
 
 	let { data } : { data: {data: PageServerData, gunsEvents: GunsEvents} } = $props();
 
 	let infoform : HTMLFormElement|undefined = $state();
 	let saved: Boolean = $state(false);
 	let saveTime: string|undefined = $state();
-	
-	let visibilitySelectedValue = $state(false);
-	const visibilityOptions: MenuOption[] = [
-		{
-			label: 'Hidden',
-			description: 'Not shown on your public page.',
-			value: false
-		},
-		{
-			label: 'Visible',
-			description: 'Shown on your public page (if its set to public).',
-			value: true
-		},
-	]; // default för visibility
-  	
-	const handleChange = () => {
-    	saved = false;
-  	};
-
-	const targetTypeOptions = [
-		{
-			value: 'one',
-			label: 'oneLabel',
-			description: 'one'
-		}
-	];
-
-	let distanceSelected = $state('meters');
-	const distanceOptions: MenuOption[] = [
-		{
-			value: "meters",
-			label: "Meters",
-			description: "Range is in meters."
-		},
-		{
-			value: "yards",
-			label: "Yards",
-			description: "Range is in yards."
-		},
-	];
-
-	/* EVENT */
-	let selectedEvent: string = $state('');
-	let eventInputContainer: HTMLDivElement|undefined = $state();
-	let eventInput: Input|undefined = $state();
-	let eventInputValue: string|undefined = $state();
-	let infoEventElement: HTMLInputElement|undefined = $state();
-	let eventOptions = $state(data.gunsEvents.events.map(event => ({
-		value: event.event.id,
-		label: event.event.name
-	})));
-	
-	eventOptions.push(
-		{
-			value: undefined,
-			label: '---',
-			// @ts-ignore
-			disabled: true
-		},
-		{
-			value: 'createEvent',
-			label: 'Create new Event',
-		}
-	);
-
-	function handleEventSelectChange(event: any): void
-	{
-		console.log('handleEventSelectChange', event);
-		if (!event.detail.value) return;
-		const selectedValue = event.detail.value;
-		console.log('selectedValue:', selectedValue)
-		if (!eventInputContainer) { console.error('No eventInputContainer found!'); return; }
-		if (!infoEventElement) { console.error('No infoEventElement found!'); return; }
-  
-		if (selectedValue === 'createEvent') {
-			event.preventDefault();
-			eventInputContainer.classList.remove('hidden');
-			infoEventElement.value = '';
-		} else {
-			if (!infoEventElement) { console.error('No infoEventElement found!'); return; }
-			console.log('Should change infoEventElement value?')
-			eventInputContainer.classList.add('hidden');
-			infoEventElement.value = '';
-			infoEventElement.value = selectedValue;
-		}
-	}
-
-	function setInfoElementValue(e: Event): void
-	{
-		if (infoEventElement && eventInputValue?.length) {
-			infoEventElement.value = eventInputValue;
-		}
-	}
-
-	// word boundary för capitalcase
-	const re = /(\b[a-z](?!\s))/;
 
 	let selectedFirearm: string = $state('');
-	const firearmsOptions = data.gunsEvents.guns.map(guns => ({
-		value: guns.gun.id,
-		label: guns.gun.name,
-		group: guns.gun.type.replace(re, (l) => {return l.toUpperCase()} ),
-		caliber: guns.gun.caliber
-	}))
-	.sort((a, b) => a.group.localeCompare(b.group));
+	let selectedAmmunition: string = $state('');
+	let targetName: string = $state('');
 	
-	function firearmChanged(e: Event): void
-	{
-		saved = false;
-		$TargetStore.info.firearm = selectedFirearm;
-	}
-
-	const ammunitionOptions = data.gunsEvents.ammunition.map(amm => ({
-		value: amm.id,
-		label: amm.name,
-		group: amm.type,
-	}))
-	.sort((a, b) => a.group.localeCompare(b.group));
-
-	function createNew(type: 'firearm'|'ammunition')
-	{
-		// TODO: open drawer with src/lib/components/gun/AddEditGun.svelte or ammunition/AddEditAmmunition
-	}
-
 	const pathExistsInInterface = (path: string): boolean => {
-		// We'll use a type checking approach based on the structure of TargetStoreInterface
 		try {
 			const parts = path.split('.');
 			let currentObj = $TargetStore;
@@ -183,32 +67,7 @@
 			prev && prev[curr] !== undefined ? prev[curr] : undefined, obj);
 	};
 	
-	// const setNestedValue = (obj: any, path: string, value: any): boolean => {
-	// 	// First check if path exists in interface
-	// 	if (!pathExistsInInterface(path)) {
-	// 		console.warn(`Path ${path} does not exist in TargetStoreInterface, skipping update`);
-	// 		return false;
-	// 	}
-		
-	// 	const parts = path.split('.');
-	// 	const lastKey = parts.pop();
-	// 	if (!lastKey) return false;
-		
-	// 	const target = parts.reduce((prev, curr) => {
-	// 		if (prev && typeof prev === 'object' && curr in prev) {
-	// 			return prev[curr];
-	// 		}
-	// 		return undefined;
-	// 	}, obj);
-		
-	// 	if (target && typeof target === 'object') {
-	// 		target[lastKey] = value;
-	// 		return target[lastKey] === value;
-	// 	}
-	// 	return false;
-	// };
-
-	function compareFormWithStore() {
+	export function compareFormWithStore() {
 		if (!infoform) return;
 		
 		// Create a copy of the current store
@@ -244,10 +103,12 @@
 			let current = updatedStore;
 			
 			for (let i = 0; i < parts.length - 1; i++) {
+				// @ts-ignore
 				current = current[parts[i]];
 			}
 			
 			const lastPart = parts[parts.length - 1];
+			// @ts-ignore
 			current[lastPart] = fieldValue;
 			hasChanges = true;
 			
@@ -267,14 +128,12 @@
 	}
 
 	onMount(() => {
-		setInterval(compareFormWithStore, 150);
-		console.log(data);
+		// setInterval(compareFormWithStore, 150);
+		console.log('infoPanel data:', data);
 	});
 
 	$effect(() => {
-		if (infoEventElement?.value) {
-			console.log('infoEventElement?.value:', infoEventElement?.value);
-		}
+		
 	});
 	
 </script>
@@ -287,341 +146,41 @@
 	>
 		<form id="infoform" bind:this={infoform} class="w-full">
 			<div class="p-4 grid grid-flow-row gap-y-2 justify-self-start place-self-start self-start w-full">
-				<input 
-					type="hidden"
-					name="info.event"
-					bind:this={infoEventElement}
-					value=""
+				<Event 
+					{data}
+					saved={false}
 				/>
-				<div class="text-sm text-primary-950 grid grid-flow-col grid-cols-[1fr_auto]">
-					<Field
-						label="Choose a previous Event or create a new Event"
-						let:id
-					>
-						<div class="w-full grid grid-flow-row">
-						<SelectField
-							{id}
-							name="eventSelect"
-							options={eventOptions}
-							placeholder="Existing events"
-							on:change={handleEventSelectChange}
-						>
-						</SelectField>
-
-						<div class="hidden" id="eventInputContainer" bind:this={eventInputContainer}>
-							<Input 
-								{id} 
-								name="eventInput"
-								placeholder="Your event name."
-								on:keypress={ () => {saved = false} }
-								bind:value={eventInputValue}
-								on:change={(e) => setInfoElementValue(e)}
-								class="border rounded p-2 mt-2"
-							/>
-						</div>
-						</div>
-					</Field>
-					<div class="ml-2">
-						<Toggle let:on={open} let:toggle let:toggleOff>
-							<Popover {open} on:close={toggleOff} placement="bottom-end" class="mt-[-1rem]">
-								<div class="px-3 py-3 bg-surface-100 border shadow text-sm max-w-48 rounded italic">
-									A group of targets or just a single target if you were in a hurry that day.
-								</div>
-							</Popover>
-							<Button
-								on:click={toggle}
-								class="p-1"
-							>
-								<CircleHelp size="16" />
-							</Button>
-						</Toggle>
-					</div>
-				</div>
 				<h3 class="mt-4">Target Information</h3>
-				<div class="text-sm text-primary-950 grid grid-flow-col grid-cols-[1fr_auto]">
-					<Field 
-						label="Name"
-						let:id
-					>
-						<Input 
-							{id} 
-							name="info.name" 
-							placeholder={ `Target #1 (${DateTime.now().toISODate()})` }
-							bind:value={$TargetStore.info.name}
-							on:keypress={ () => {saved = false} }
-							required
-						/>
-					</Field>
-					<div class="ml-2">
-						<Toggle let:on={open} let:toggle let:toggleOff>
-							<Popover {open} on:close={toggleOff} placement="bottom-end" class="mt-[-1rem]">
-								<div class="px-3 py-3 bg-surface-100 border shadow text-sm max-w-48 rounded italic">
-									A friendly name for the target so you can find it later.
-								</div>
-							</Popover>
-							<Button
-								on:click={toggle}
-								class="p-1"
-							>
-								<CircleHelp size="16" />
-							</Button>
-						</Toggle>
-					</div>
-				</div>
-				<div class="text-sm text-primary-950 grid grid-flow-col grid-cols-[1fr_auto] opacity-40">
-					<Field
-						label="Target Type"
-						let:id
-					>
-						<SelectField
-							options={targetTypeOptions}
-							disabled
-							name="type"
-							on:change={ () => {saved = false} }
-						>
-						</SelectField>
-					</Field>
-					<div class="ml-2">
-						<Toggle let:on={open} let:toggle let:toggleOff>
-							<Popover {open} on:close={toggleOff} placement="bottom-end" class="mt-[-1rem]">
-								<div class="px-3 py-3 bg-surface-100 border shadow text-sm max-w-48 rounded italic">
-									Used for automatic scoring of your target. (Coming Soon&trade;)
-								</div>
-							</Popover>
-							<Button
-								on:click={toggle}
-								class="p-1"
-							>
-								<CircleHelp size="16" />
-							</Button>
-						</Toggle>
-					</div>
-				</div>
-				<div class="text-sm text-primary-950 grid grid-flow-col grid-cols-[1fr_auto]">
-					<Field
-						label="Distance"
-						let:id
-					>
-						<Input 
-							{id} 
-							name="target.range" 
-							placeholder=80
-							type="number"
-							on:keypress={ () => {saved = false} }
-							required
-						/>
-						<SelectField
-							name="target.rangeUnit"
-							options={distanceOptions}
-							bind:value={distanceSelected}
-							onchange={ () => {saved = false} }
-							class="max-w-36"
-						>
-						</SelectField>
-					</Field>
-					<div class="ml-2">
-						<Toggle let:on={open} let:toggle let:toggleOff>
-							<Popover {open} on:close={toggleOff} placement="bottom-end" class="mt-[-1rem]">
-								<div class="px-3 py-3 bg-surface-100 border shadow text-sm max-w-48 rounded italic">
-									Distance to target.
-								</div>
-							</Popover>
-							<Button
-								on:click={toggle}
-								class="p-1"
-							>
-								<CircleHelp size="16" />
-							</Button>
-						</Toggle>
-					</div>
-				</div>
-				<div class="text-sm text-primary-950 grid grid-flow-col grid-cols-[1fr_auto]">
-					<Field
-						label="Firearm"
-						let:id
-						placeholder="Select firearm"
-					>
-						<SelectField
-							bind:value={selectedFirearm}
-							options={firearmsOptions}
-							name="info.firearm"
-						>
-							<MenuItem
-								slot="option"
-								let:option
-								let:index
-								let:selected
-								let:highlightIndex
-								class={cls(
-									index === highlightIndex && "bg-surface-content/5",
-									option === selected && "font-semibold",
-									option.label ? "px-4" : "px-2",
-								)}
-								scrollIntoView={index === highlightIndex}
-							>
-								<div>
-									<div class="">{option.label}</div>
-									<div class="text-sm text-surface-content/50">
-										{option.group} {option.caliber}
-									</div>
-								</div>
-							</MenuItem>
-						</SelectField>
-						<Button 
-							class="ml-2 p-1"
-							title="Add new firearm"
-							on:click={ () => createNew('firearm') }
-						>
-							<PlusSquare />
-						</Button>
-					</Field>
-					<div class="ml-2">
-						<Toggle let:on={open} let:toggle let:toggleOff>
-							<Popover {open} on:close={toggleOff} placement="bottom-end" class="mt-[-1rem]">
-								<div class="px-3 py-3 bg-surface-100 border shadow text-sm max-w-48 rounded italic">
-									Choose a firearm or create a new firearm.
-								</div>
-							</Popover>
-							<Button
-								on:click={toggle}
-								class="p-1"
-							>
-								<CircleHelp size="16" />
-							</Button>
-						</Toggle>
-					</div>
-				</div>
-				<div class="text-sm text-primary-950 grid grid-flow-col grid-cols-[1fr_auto]">
-					<Field
-						label="Ammunition"
-						let:id
-						placeholder="Select firearm"
-					>
-						<SelectField
-							onchange={ () => {saved = false} }
-							options={ammunitionOptions}
-							name="info.ammunition"
-						>
-							<MenuItem
-								slot="option"
-								let:option
-								let:index
-								let:selected
-								let:highlightIndex
-								class={cls(
-									index === highlightIndex && "bg-surface-content/5",
-									option === selected && "font-semibold",
-									option.label ? "px-4" : "px-2",
-								)}
-								scrollIntoView={index === highlightIndex}
-							>
-								<div>
-									<div class="">{option.label}</div>
-									<div class="text-sm text-surface-content/50">
-										{option.group} {option.caliber}
-									</div>
-								</div>
-							</MenuItem>
-						</SelectField>
-						<Button 
-							class="ml-2 p-1"
-							title="Add new ammunition"
-							on:click={ () => createNew('ammunition') }
-						>
-							<PlusSquare />
-						</Button>
-					</Field>
-					<div class="ml-2">
-						<Toggle let:on={open} let:toggle let:toggleOff>
-							<Popover {open} on:close={toggleOff} placement="bottom-end" class="mt-[-1rem]">
-								<div class="px-3 py-3 bg-surface-100 border shadow text-sm max-w-48 rounded italic">
-									Choose ammunition or create a new ammuntion entry.
-								</div>
-							</Popover>
-							<Button
-								on:click={toggle}
-								class="p-1"
-							>
-								<CircleHelp size="16" />
-							</Button>
-						</Toggle>
-					</div>
-				</div>
 				
-				<div class="text-sm text-primary-950 grid grid-flow-col grid-cols-[1fr_auto]">
-					<TextField
-						label="Note"
-						name="info.note"
-						multiline
-						onchange={ () => {saved = false} }
-						placeholder="Today was a nice day... "
-					></TextField>
-					<div class="ml-2">
-						<Toggle let:on={open} let:toggle let:toggleOff>
-							<Popover {open} on:close={toggleOff} placement="bottom-end" class="mt-[-1rem]">
-								<div class="px-3 py-3 bg-surface-100 border shadow text-sm max-w-48 rounded italic">
-									Any useful notes... 
-								</div>
-							</Popover>
-							<Button
-								on:click={toggle}
-								class="p-1"
-							>
-								<CircleHelp size="16" />
-							</Button>
-						</Toggle>
-					</div>
-				</div>
-				<div class="text-sm text-primary-950 grid grid-flow-col grid-cols-[1fr_auto]">
-					<Field
-						label="Visibility"
-						let:id
-					>
-						<SelectField
-							name="info.public"
-							options={visibilityOptions}
-							bind:value={visibilitySelectedValue}
-							on:change={handleChange}
-						>
-						<MenuItem
-							slot="option"
-							let:option
-							let:index
-							let:selected
-							let:highlightIndex
-							class={cls(
-								index === highlightIndex && "bg-surface-content/5",
-								option === selected && "font-semibold",
-								option.label ? "px-4" : "px-2",
-							)}
-							scrollIntoView={index === highlightIndex}
-						>
-							<div>
-								<div class="">{option.label}</div>
-								<div class="text-sm text-surface-content/50">
-									{option.group} {option.caliber}
-								</div>
-							</div>
-						</MenuItem>
-					</SelectField>
+				<Name 
+					saved={false}
+					value=''
+				/>
 
-					</Field>
-					<div class="ml-2">
-						<Toggle let:on={open} let:toggle let:toggleOff>
-							<Popover {open} on:close={toggleOff} placement="bottom-end" class="mt-[-1rem]">
-								<div class="px-3 py-3 bg-surface-100 border shadow text-sm max-w-48 rounded italic">
-									Display your best targets on your public page (if your profile is set to public).
-								</div>
-							</Popover>
-							<Button
-								on:click={toggle}
-								class="p-1"
-							>
-								<CircleHelp size="16" />
-							</Button>
-						</Toggle>
-					</div>
-				</div>
+				<TargetType
+					saved={false}
+				/>
+				
+				<Distance
+					saved={false}
+				/>
+				
+				<Firearm
+					{data}
+					selected={selectedFirearm}
+				/>
+
+				<Ammunition 
+					{data}
+				/>
+				
+				<Note 
+					saved={false}
+				/>
+
+				<Visibility 
+					saved={false}
+				/>
 			</div>
 		</form>		
 		<div class="w-full px-4 py-4 pb-8 grid grid-cols-1 items-end place-content-end mr-8">
